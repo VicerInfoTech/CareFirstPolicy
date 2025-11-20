58fec32f31572c53b19684be853fe514ad3be71b
Amit Soni <>
2025-11-20 17:03:23 +0530

    add training menu

---
+++ CFP.Web/Controllers/TrainingController.cs
@@ -0,0 +1,26 @@
+﻿using CFP.Common.Utility;
+using CFP.Patient.Controllers;
+using CFP.Provider.IProvider;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CFP.Web.Controllers
+{
+    public class TrainingController : BaseController
+    {
+        #region Variables
+
+        public IWebHostEnvironment _webHostEnvironment { get; }
+        #endregion
+
+        #region Constructor
+        public TrainingController(ICommonProvider commonProvider, ISessionManager sessionManager, IWebHostEnvironment webHostEnvironment) : base(commonProvider, sessionManager)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        #endregion
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
